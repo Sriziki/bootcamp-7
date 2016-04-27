f036f52ad93a452520eb00bb040ae2ffba48f6f0
Sophie Riziki <sophieriziki@gmail.com>
2016-04-27 10:35:04 +0300

    data_types function

---
+++ day_3/data_types.py
@@ -0,0 +1,26 @@
+def data_type(x):
+	'''
+	Takes in an argument, x:
+	- For an integer, return x ** 2
+	- For a float, return x / 2
+	- For a string, returns "hello" + x
+	- For a boolean, return "boolean"
+	- For a long, return squareroot(x)
+	'''
+
+	if type (x) == int:
+		return x ** 2
+
+	elif type(x) == float:
+	    return x / 2
+
+	elif type (x) == str:
+	    return ("hello " + x)
+
+	elif type(x) == bool:
+	    return "boolean" 
+
+	elif type(x) == long:
+	    return "long"            	
+
+print (data_type("sophie"))
